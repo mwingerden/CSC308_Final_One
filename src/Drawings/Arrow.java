3d8ba56707ccc4028dc832938c51f5b788147fa1
Matthew Wingerden <matthew.wingerden@gmail.com>
2023-03-16 19:37:43 -0700

    Added Drawable Arrow Head.

--- src/Drawings/Arrow.java
+++ src/Drawings/Arrow.java
@@ -3,6 +3,7 @@ package Drawings;
 import Drawings.Blocks.CodeBlock;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +32,18 @@ public class Arrow implements Draw {
         g.setColor(color);
         Graphics2D graphics2D = (Graphics2D) g;
         graphics2D.draw(new Line2D.Float(x1, y1, x2, y2));
+        Polygon arrowHead = new Polygon();
+        AffineTransform tx = new AffineTransform();
+        arrowHead.addPoint( 0,5);
+        arrowHead.addPoint( -5, -5);
+        arrowHead.addPoint( 5,-5);
+        tx.setToIdentity();
+        double angle = Math.atan2(y2 - y1, x2 - x1);
+        tx.translate(x1 + ((double)(x2 - x1) / 2), y1 + ((double)(y2 - y1) / 2));
+        tx.rotate((angle-Math.PI/2d));
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setTransform(tx);
+        g2d.fill(arrowHead);
     }
 
     @Override
